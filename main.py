be2b876d8838d91408f119dbb79eac35a970764b
everaldo@bb-8 <juniorklawa10@gmail.com>
2023-06-12 16:49:06 -0300

    feat: added test and training data

--- main.py
+++ main.py
@@ -2,20 +2,41 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 import os
+import shutil
 
+# Create separate directories for training and testing images
+def create_train_test_dirs(base_path, train_path, test_path):
+    os.makedirs(train_path, exist_ok=True)
+    os.makedirs(test_path, exist_ok=True)
 
-# Split the dataset into 70% train set and 30% test set
-def split_data(path):
-    all_images = os.listdir(path)
-    train_images, test_images = train_test_split(all_images, test_size=0.3, random_state=42)
-    return train_images, test_images
+    flower_folders = os.listdir(base_path)
 
+    for folder in flower_folders:
+        train_flower_path = os.path.join(train_path, folder)
+        test_flower_path = os.path.join(test_path, folder)
+        
+        os.makedirs(train_flower_path, exist_ok=True)
+        os.makedirs(test_flower_path, exist_ok=True)
 
-lily_train, lily_test = split_data('./flower_images/Lilly')
-lotus_train, lotus_test = split_data('./flower_images/Lotus')
-orchid_train, orchid_test = split_data('./flower_images/Orchid')
-sunflower_train, sunflower_test = split_data('./flower_images/Sunflower')
-tulip_train, tulip_test = split_data('./flower_images/Tulip')
+        flower_images = os.listdir(os.path.join(base_path, folder))
+        train_images, test_images = train_test_split(flower_images, test_size=0.3, random_state=42)
+
+        for train_image in train_images:
+            src = os.path.join(base_path, folder, train_image)
+            dst = os.path.join(train_flower_path, train_image)
+            shutil.copy(src, dst)
+
+        for test_image in test_images:
+            src = os.path.join(base_path, folder, test_image)
+            dst = os.path.join(test_flower_path, test_image)
+            shutil.copy(src, dst)
+
+base_path = './flower_images'
+train_path = './train_images'
+test_path = './test_images'
+
+# Create training and testing directories
+create_train_test_dirs(base_path, train_path, test_path)
 
 train_datagen = ImageDataGenerator(
     rescale=1./255,
@@ -26,9 +47,8 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-# Create train and test generator using the split image file names
 train_generator = train_datagen.flow_from_directory(
-    './flower_images',
+    train_path,
     target_size=(224, 224),
     batch_size=32,
     class_mode='categorical',
@@ -36,7 +56,7 @@ train_generator = train_datagen.flow_from_directory(
 )
 
 test_generator = test_datagen.flow_from_directory(
-    './flower_images',
+    test_path,
     target_size=(224, 224),
     batch_size=32,
     class_mode='categorical',
@@ -69,7 +89,6 @@ model.save('flower_classifier_model')
 
 loaded_model = tf.keras.models.load_model('flower_classifier_model')
 
-
 def classify_image(image_path):
     img = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
     img_array = tf.keras.preprocessing.image.img_to_array(img)
@@ -84,8 +103,7 @@ def classify_image(image_path):
 
     return predicted_class, predicted_percentage
 
-
-image_path = './flower_images/Lotus/0cd4d2960b.jpg'
+image_path = './test_images/Lotus/some_lotus_image.jpg'  # Replace 'some_lotus_image.jpg' with the correct file name
 predicted_class, predicted_percentage = classify_image(image_path)
 print(f"The predicted class for {image_path} is: {predicted_class}")
 print(f"The percentage of the predicted class is: {predicted_percentage:.2f}%")
